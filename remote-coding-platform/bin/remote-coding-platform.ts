23c5d4b162017dacabdcb7813abf80afba8fd162
GeorgianBadita <geo.badita@gmail.com>
2021-01-31 22:40:01 +0000

    CDK changes

--- remote-coding-platform/bin/remote-coding-platform.ts
+++ remote-coding-platform/bin/remote-coding-platform.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { RemoteCodingPlatformStack } from '../lib/remote-coding-platform-stack';
+import "source-map-support/register";
+import * as cdk from "@aws-cdk/core";
+import { RemoteCodingPlatformStack } from "../lib/stacks/remote-coding-platform-stack";
 
 const app = new cdk.App();
-new RemoteCodingPlatformStack(app, 'RemoteCodingPlatformStack');
+new RemoteCodingPlatformStack(app, "RemoteCodingPlatformStack");
---
+++ remote-coding-platform/lib/stacks/remote-coding-platform-stack.ts
@@ -0,0 +1,9 @@
+import { Stack, Construct, StackProps } from "@aws-cdk/core";
+
+export class RemoteCodingPlatformStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    //Bucket for saving problems data ()
+  }
+}
